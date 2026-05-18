3b79e947436067733e071e4b70975a996d145f70
Aryan Jaiswal <118985850+Aryan8400@users.noreply.github.com>
2026-05-18 14:30:37 +0000

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { DataProvider } from "./context/DataContext.jsx";
@@ -9,7 +9,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <AuthProvider>
           <DataProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById("root")).render(
           </DataProvider>
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
